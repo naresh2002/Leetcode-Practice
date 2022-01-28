fb9b28d678f96c851f1e0751ce2624666d84bcc8
Naresh <56593631+naresh2002@users.noreply.github.com>
2022-01-29 01:34:04 +0530

    Time: 106 ms (58.83%), Space: 31.5 MB (96.36%) - LeetHub

---
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,40 @@
+class WordDictionary {
+  unordered_map<int,vector<string>> words;
+  
+public:
+  bool isEqual(string word1,string word2) {
+    for(int i=0;i<word1.size();i++) {
+      if(word2[i]=='.' or word1[i]=='.') {
+        continue;
+      }
+      if(word1[i]!=word2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  WordDictionary() {
+    
+  }
+
+  void addWord(string word) {
+    words[word.size()].push_back(word);
+  }
+
+  bool search(string word) {
+    for(auto it:words[word.size()]) {
+      if(isEqual(it,word)) {
+        return true;
+      }
+    }
+    return false;
+  }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
