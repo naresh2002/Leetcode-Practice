e47722f99d00edfde46a6f2fbd9c04f5b900400b
Naresh <56593631+naresh2002@users.noreply.github.com>
2022-07-19 12:37:42 +0530

    Time: 34 ms (36.16%), Space: 18 MB (18.76%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int s = 0;
+        for (auto x : nums) {
+            s += x;
+        }
+        return ((nums.size() * (nums.size() + 1)) / 2) - s;
+    }
+};
