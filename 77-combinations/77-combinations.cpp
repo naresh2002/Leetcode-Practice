d26d5e0bf15edce1e1083157b9ec1b8d5729d793
Naresh <56593631+naresh2002@users.noreply.github.com>
2022-09-24 21:39:24 +0530

    Time: 48 ms (46.50%), Space: 10 MB (53.34%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    // vector<bool> vis;
+    vector<bool> vis;
     void dfs (int curr, int n, int k, vector<int> &v) {
         if (v.size() == k) {
             ans.push_back(v);
             return;
         }
         for (int i = curr; i <= n; i++) {
-            // if (vis[i]) {
-            //     continue;
-            // }
-            // vis[i] = true;
+            if (vis[i]) {
+                continue;
+            }
+            vis[i] = true;
             v.push_back(i);
             dfs(i + 1, n, k, v);
-            // vis[i] = false;
+            vis[i] = false;
             v.pop_back();
         }
     }
     
     vector<vector<int>> combine(int n, int k) {
         vector<int> v;
-        // vis.resize(n + 1, false);
+        vis.resize(n + 1, false);
         dfs(1, n, k, v);
         return ans;
     }
