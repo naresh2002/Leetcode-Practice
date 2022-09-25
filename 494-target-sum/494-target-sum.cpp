54ca2950caacf1f83b49b8559be388992b2d7260
Naresh <56593631+naresh2002@users.noreply.github.com>
2022-09-25 21:18:34 +0530

    Time: 10 ms (92.65%), Space: 9.3 MB (56.63%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
-    int dfs (int curr, int tot, vector<int> & v, int target) {
-        if (curr == v.size()) {
-            if (tot == target) {
-                return 1;
-            }
-            return 0;
-        }
-        int a = dfs(curr + 1, tot + v[curr], v, target);
-        int b = dfs(curr + 1, tot - v[curr], v, target);
-        return a + b;
-    }
+    // int dfs (int curr, int tot, vector<int> & v, int k) {
+    //     if (tot == k) {
+    //         return 1;
+    //     }
+    //     if (curr == v.size()) {
+    //         return 0;
+    //     }
+    //     int a = dfs(curr + 1, tot + v[curr], v, k);
+    //     int b = dfs(curr + 1, tot, v, k);
+    //     return a + b;
+    // }
     
     int findTargetSumWays(vector<int>& v, int target) {
-        return dfs(0, 0, v, target);
+        int s = 0;
+        for (auto x : v) {
+            s += x;
+        }
+        int n = v.size(), new_target = (s + target) / 2;
+        if (s < target || (target + s) % 2 || new_target < 0) {
+            return 0;
+        }
+        // return dfs(0, 0, v, (s + target) / 2);
+        /*  S1 - S2 = target;
+            S1 + S2 = sum(v);
+            Adding Above Two :
+            => 2(S1) = target + sum(v)
+        */
+        vector<int> dp(new_target + 1, 0);
+        dp[0] = 1;
+        for(int i = 0; i < n; i++)
+            for(int j = new_target; j >= v[i]; j--)
+                dp[j] += dp[j-v[i]];
+        return dp[new_target];
     }
 };
