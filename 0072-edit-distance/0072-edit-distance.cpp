a55073cb35cec0f1d9933530e19033f08f5d3732
Naresh <56593631+naresh2002@users.noreply.github.com>
2022-12-16 20:21:44 +0530

    Time: 20 ms (65.48%), Space: 9.1 MB (41.51%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -5,13 +5,11 @@ public:
         int m = word2.size();
         vector<vector<int>> dp (n + 1, vector<int> (m + 1));
         for (int i = 0; i <= n; i++) {
-            dp[i][0] = i;
-        }
-        for (int i = 0; i <= m; i++) {
-            dp[0][i] = i;
-        }
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
+            for (int j = 0; j <= m; j++) {
+                if (!i || !j) {
+                    dp[i][j] = max(i, j);
+                    continue;
+                }
                 if (word1[i - 1] == word2[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1];
                 }
