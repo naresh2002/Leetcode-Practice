186a61b81d5051704c5ebf7159db3d820fbf91ba
Naresh <56593631+naresh2002@users.noreply.github.com>
2022-12-20 08:26:22 +0530

    Time: 1222 ms (5.03%), Space: 122.7 MB (5.18%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    map<TreeNode*, vector<long long>> m;
+    void solve(TreeNode *root, TreeNode *parent) {
+        if (!root) {
+            return;
+        }
+        m[root].push_back(root -> val);
+        for (auto x : m[parent]) {
+            if ((x + root -> val) < INT_MIN || (x + root -> val) > INT_MAX) {
+                continue;
+            }
+            m[root].push_back(x + root -> val);
+        }
+        solve(root -> left, root);
+        solve(root -> right, root);
+    }
+    
+    int pathSum(TreeNode* root, int targetSum) {
+        if (!root) {
+            return 0;
+        }
+        m[root].push_back(root -> val);
+        solve(root -> left, root);
+        solve(root -> right, root);
+        map<int, int> cnt;
+        for (auto x : m) {
+            for (auto y : m[x.first]) {
+                cnt[y]++;
+            }
+        }
+        for (auto x : cnt) {
+            cout << x.first << " " << x.second << endl;
+        }
+        return cnt[targetSum];
+    }
+};
