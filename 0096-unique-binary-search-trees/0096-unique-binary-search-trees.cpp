bcb870c2e429eaa0668867ff3743d60decb5e28e
Naresh <56593631+naresh2002@users.noreply.github.com>
2023-01-08 22:02:20 +0530

    Time: 3 ms (41.57%), Space: 5.9 MB (75.96%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -10,4 +10,18 @@ public:
         }
         return dp[n];
     }
+
+/*
+Approach (CATALAN Number):
+We can find the number of BST’s by thinking in recursive manner for ease:
+
+1. Choose 1 as root, no element present on left-subtree. N-1 elements present on the right subtree.
+
+2. Choose 2 as root, 1 element present on the left-subtree. N-2 elements present on the right-subtree.
+
+3. Similarly, Choose i as root, i-1 elements present on the left-subtree. N-i elements on the right-subtree.
+
+Then, Total number of BST’s C(N) = C(0)C(N-1) + C(1)C(N-2) +………+ C(i-1)C(N-i) +………….+ C(N-1)C(0).
+where C(i) is the number of unique BST with i nodes.
+*/
 };
