59ff127991a3b817242cb492cbb75e8f45943f15
Naresh <naresh6436@gmail.com>
2023-02-05 22:47:58 +0530

    Time: 24 ms (82.62%), Space: 10.6 MB (93.07%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int> cnt (26, 0);
+        for (auto x : s) {
+            cnt[x - 'a']++;
+        }
+        for (int i = 0; i < s.size(); i++) {
+            if (cnt[s[i] - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
