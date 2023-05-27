6359ba021a28e076037c198c2038600df1343fce
Naresh <naresh6436@gmail.com>
2023-05-27 22:42:37 +0530

    Time: 29 ms (24.10%), Space: 9.1 MB (41.30%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-        vector<vector<int>> dp (n + 1, vector<int> (m + 1));
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= m; j++) {
+        int m = word1.size();
+        int n = word2.size();
+        vector<vector<int>> dp(m + 1, vector<int> (n + 1));
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
                 if (!i || !j) {
                     dp[i][j] = max(i, j);
-                    continue;
                 }
-                if (word1[i - 1] == word2[j - 1]) {
+                else if (word1[i - 1] == word2[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1];
                 }
                 else {
-                    int insertChar = dp[i][j - 1] + 1;
-                    int deleteChar = dp[i - 1][j] + 1;
+                    int insertChar = dp[i - 1][j] + 1;
+                    int deleteChar = dp[i][j - 1] + 1;
                     int replaceChar = dp[i - 1][j - 1] + 1;
                     dp[i][j] = min({insertChar, deleteChar, replaceChar});
                 }
             }
         }
-        return dp[n][m];
+        return dp[m][n];
     }
 };
