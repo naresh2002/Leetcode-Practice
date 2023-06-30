3d005532bb3582d57f275bf2b74eb58b474a6766
Naresh <naresh6436@gmail.com>
2023-06-30 22:55:11 +0530

    Time: 38 ms (5.12%), Space: 9 MB (35.94%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.cpp
+++ 0174-dungeon-game/0174-dungeon-game.cpp
@@ -1,49 +1,32 @@
 class Solution {
 public:
-    bool passageAvailable(int k, vector<vector<int>> &minHealth, int n, int m) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (minHealth[i - 1][j] < -k && minHealth[i][j - 1] < -k) {
-                    minHealth[i][j] = -1e8;
-                }
-            }
-        }
-        if (minHealth[n][m] < -k) {
-            return false;
-        }
-        return true;
-    }
     
     bool isValid(int k, vector<vector<int>>& dungeon, vector<vector<int>> &minHealth) {
         int n = dungeon.size();
         int m = dungeon[0].size();
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= m; j++) {
-                if (!i || !j) {
-                    if (i != 1) {
-                        minHealth[i][j] = -1e8;
-                    }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!i && !j) {
+                    minHealth[i][j] = (dungeon[i][j] < -k ? -1e9 : dungeon[i][j]);
+                    continue;
                 }
-                else {
-                    int up = minHealth[i - 1][j];
-                    int left = minHealth[i][j - 1];
-                    minHealth[i][j] = max(up, left) + dungeon[i - 1][j - 1];
-                    if (minHealth[i][j] < -k) {
-                        minHealth[i][j] = -1e8;
-                    }
-                }
+               int ans = -1e9;
+                if (i && minHealth[i - 1][j] != -1e9)   ans = minHealth[i - 1][j];
+                if (j && minHealth[i][j - 1] != -1e9) ans = max(ans, minHealth[i][j - 1]);
+                minHealth[i][j] = max(int(-1e9), ans + dungeon[i][j]);
+              if (minHealth[i][j] < -k) minHealth[i][j] = -1e9;
             }
         }
-        return minHealth[n][m] >= -k;
+       
+        return minHealth[n - 1][m - 1] >= -k;
     }
     
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
         int n = dungeon.size();
         int m = dungeon[0].size();
-        int l = 0, r = 4e7;
+        int l = 0, r = 1e9;
         int ans = r;
         vector<vector<int>> minHealth(n + 1, vector<int> (m + 1));
-        minHealth[1][0] = 0;
         while (l <= r) {
             int mid = l + (r - l) / 2;
             if (isValid(mid, dungeon, minHealth)) {
