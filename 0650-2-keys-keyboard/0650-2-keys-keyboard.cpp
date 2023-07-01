65ae8516340968897197aba65fe654e07407eba0
Naresh <naresh6436@gmail.com>
2023-07-01 14:30:36 +0530

    Time: 27 ms (38.66%), Space: 6.7 MB (33.19%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -5,7 +5,7 @@ public:
         dp[1] = 0;
         for (int i = 2; i <= n; i++) {
             dp[i] = i;
-            for (int j = 1; j < i; j++) {
+            for (int j = 1; j <= i / 2; j++) {
                 if (i % j == 0) {
                     dp[i] = min(dp[i], dp[j] + (i / j));
                 }
