22ad421fa9188925cf7fa0eaea796059006e0fbc
Naresh <naresh6436@gmail.com>
2023-07-24 19:37:32 +0530

    Time: 32 ms (16.78%), Space: 15.4 MB (8.23%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> adj[2005];       // Array of vector
+    unordered_map<int, int> vis;
+    unordered_map<int, int> done;
+    
+    bool cyclic(int n) {
+        if (done[n]) {
+            return false;
+        }
+        vis[n] = done[n] = 1;
+        bool cycle = false;
+        for (auto x : adj[n]) {
+            if (vis[x]) {
+                cycle = true;
+            }
+            else {
+                if (cyclic(x)) {
+                    cycle = true;
+                }
+            }
+            if (cycle) {
+                break;
+            }
+        }
+        vis[n] = 0;
+        return cycle;
+    }
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        for (auto x : prerequisites) {
+            adj[x[0]].push_back(x[1]);
+        }
+        for (int i = 0; i < numCourses; i++) {
+            if (cyclic(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
