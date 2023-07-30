4ea264488058fbee4c6e8fc235e90789b2fb5462
Naresh <naresh6436@gmail.com>
2023-07-30 18:11:04 +0530

    Time: 71 ms (22.94%), Space: 30.9 MB (9.15%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -0,0 +1,111 @@
+class Solution {
+public:
+    int n, m;
+    vector<vector<int>> solve(queue<vector<int>> &q, vector<vector<int>> &vis, vector<vector<int>>& heights) {
+        vector<vector<int>> v;
+        while (q.size()) {
+            int x = q.front()[0];
+            int y = q.front()[1];
+            if (x + 1 < n && vis[x + 1][y] == 0 && heights[x + 1][y] >= heights[x][y]) {
+                q.push({x + 1, y});
+                vis[x + 1][y] = 1;
+            }
+            if (x - 1 >= 0 && vis[x - 1][y] == 0 && heights[x - 1][y] >= heights[x][y]) {
+                q.push({x - 1, y});
+                vis[x - 1][y] = 1;
+            }
+            if (y + 1 < m && vis[x][y + 1] == 0 && heights[x][y + 1] >= heights[x][y]) {
+                q.push({x, y + 1});
+                vis[x][y + 1] = 1;
+            }
+            if (y - 1 >= 0 && vis[x][y - 1] == 0 && heights[x][y - 1] >= heights[x][y]) {
+                q.push({x, y - 1});
+                vis[x][y - 1] = 1;
+            }
+            v.push_back(q.front());
+            // cout << x << " " << y << " -> " << pacific.size() << endl;
+            q.pop();
+        }
+        return v;
+    }
+    
+    // vector<vector<int>> atlanticPossible(vector<vector<int>>& heights) {
+    //     queue<vector<int>> q;
+    //     vector<vector<int>> atlantic;
+    //     vector<vector<int>> vis(n, vector<int> (m, 0));
+    //     for (int i = 0; i < n; i++) {
+    //         q.push({i, m - 1});
+    //         vis[i][m - 1] = 1;
+    //     }
+    //     for (int i = 0; i < m; i++) {
+    //         q.push({n - 1, i});
+    //         vis[n - 1][i] = 1;
+    //     }
+    //     while (q.size()) {
+    //         int x = q.front()[0];
+    //         int y = q.front()[1];
+    //         if (x + 1 < n && vis[x + 1][y] == 0 && heights[x + 1][y] >= heights[x][y]) {
+    //             q.push({x + 1, y});
+    //             vis[x + 1][y] = 1;
+    //         }
+    //         if (y - 1 >= 0 && vis[x][y - 1] == 0 && heights[x][y - 1] >= heights[x][y]) {
+    //             q.push({x, y - 1});
+    //             vis[x][y - 1] = 1;
+    //         }
+    //         atlantic.push_back(q.front());
+    //         q.pop();
+    //     }
+    //     return atlantic;
+    // }
+    
+
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        n = heights.size();
+        m = heights[0].size();
+        queue<vector<int>> q;
+        vector<vector<int>> vis(n, vector<int> (m, 0));
+        
+        // -------------------PACIFIC-------------------
+        for (int i = 0; i < n; i++) {
+            q.push({i, 0});
+            vis[i][0] = 1;
+        }
+        for (int i = 1; i < m; i++) {   // i = 1 as [0,0] should not occur twice
+            q.push({0, i});
+            vis[0][i] = 1;
+        }        
+        vector<vector<int>> pacific = solve(q, vis, heights);
+        // ----------------PACIFIC [END]----------------
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                vis[i][j] = 0;      // Reset vis to 0 [ERROR with memset for 2D vector]
+            }
+        }
+        
+        // -------------------ATLANTIC-------------------
+        for (int i = 0; i < n; i++) {
+            q.push({i, m - 1});
+            vis[i][m - 1] = 1;
+        }
+        for (int i = 0; i < m - 1; i++) {   // i < m - 1 as [n - 1, m - 1] should not occur twice
+            q.push({n - 1, i});
+            vis[n - 1][i] = 1;
+        }
+        vector<vector<int>> atlantic = solve(q, vis, heights);
+        // ----------------ATLANTIC [END]----------------
+        
+        
+        vector<vector<int>> ans;
+        map<vector<int>, int> m;
+        for (auto x : pacific) {
+            m[x]++;
+        }
+        for (auto x : atlantic) {
+            if (m.count(x)) {
+                ans.push_back(x);
+            }
+        }
+        return ans;
+    }
+};
