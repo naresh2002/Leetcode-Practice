34767d927aafe2cf6f7f1d004bd2cfc14d1c10fd
Naresh <naresh6436@gmail.com>
2024-07-08 18:25:49 +0530

    3 ms 11.8 MB

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,16 +1,54 @@
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        int n = s.size();
-        vector<bool> dp(n + 1, false);  // 1-based INDEXING
-        dp[0] = true;
-        for (int j = 0; j < n; j++) {   // ENDING point
-            for (int i = 0; i <= j; i++) {   // STARTING point
-                if (dp[i] && find(wordDict.begin(), wordDict.end(), s.substr(i, j - i + 1)) != wordDict.end()) {
-                    dp[j + 1] = true;
-                }
+    struct TrieNode {
+        TrieNode* child[26];
+        bool wordEnd;
+        
+        TrieNode() {
+            wordEnd = false;
+            for (int i = 0; i < 26; i++) {
+                child[i] = NULL;
             }
         }
-        return dp[n];
+    };
+    
+    void insertTrie(TrieNode* node, string s) {
+        TrieNode* currNode = node;
+        for (auto ch : s) {
+            if (currNode -> child[ch - 'a'] == NULL) {
+                currNode -> child[ch - 'a'] = new TrieNode();
+            }
+            currNode = currNode -> child[ch - 'a'];
+        }
+        currNode -> wordEnd = true;
+    }
+    
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+        TrieNode* node = new TrieNode();
+        for (auto x : wordDict) {
+            insertTrie(node, x);
+        }
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        for (int i = 0; i <= n; i++) {
+            if (dp[i] == 0) {
+                continue;
+            }
+            TrieNode* p = node;
+            int j = i;
+            while (j < n && p -> child[s[j] - 'a']) {
+                p = p -> child[s[j] - 'a'];
+                j++;
+                if (p -> wordEnd) {
+                    dp[j] = 1;  // 1 based indexing
+                }
+            }
+            if (dp[n]) {
+                return true;
+            }
+        }
+        return false;
     }
 };
