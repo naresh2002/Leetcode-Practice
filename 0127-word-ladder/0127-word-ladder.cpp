6fa88949f69b49867471683108a19882a0514772
Naresh <naresh6436@gmail.com>
2024-10-06 16:32:36 +0530

    Time: 70 ms (64.58%), Space: 19.6 MB (81.01%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // https://leetcode.com/problems/word-ladder/discuss/5864616/Easy-To-Understand-BFS-Solution
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         unordered_set<string> wordSet (wordList.begin(), wordList.end());
         if (wordSet.find(endWord) == wordSet.end()) {
