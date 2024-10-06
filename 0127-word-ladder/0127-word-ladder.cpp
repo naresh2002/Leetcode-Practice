f9728b599f9af5386ee245da639f40722d29413e
Naresh <naresh6436@gmail.com>
2024-10-06 16:32:16 +0530

    Time: 78 ms (59.31%), Space: 19.6 MB (72.34%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> wordSet (wordList.begin(), wordList.end());
+        if (wordSet.find(endWord) == wordSet.end()) {
+            return 0;
+        }
+        queue<pair<string, int>> q;
+        q.push({beginWord, 1});
+        while (!q.empty()) {
+            auto [currWord, steps] = q.front();
+            q.pop();
+            
+            for (int i = 0; i < currWord.size(); i++) {
+                char originalChar = currWord[i];
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    if (ch == originalChar) {
+                        continue;
+                    }
+                    currWord[i] = ch;
+                    
+                    if (currWord == endWord) {
+                        return steps + 1;
+                    }
+                    
+                    if (wordSet.find(currWord) != wordSet.end()) {
+                        q.push({currWord, steps + 1});
+                        wordSet.erase(currWord);
+                    }
+                }
+                currWord[i] = originalChar;
+            }
+        }
+        return 0;
+    }
+};
