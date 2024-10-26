3188dd25f89788b034cf4356366fbef66bf6af4e
Naresh <naresh6436@gmail.com>
2024-10-26 22:45:57 +0530

    Time: 0 ms (100.00%), Space: 18 MB (99.81%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,45 @@
+class Solution { // DISJOINT SET UNION
+    int parent[201];
+    
+    void make_set (int n) {
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+    
+    int find_set(int a) {
+        if (parent[a] == a) {
+            return a;
+        }
+        parent[a] = find_set(parent[a]);
+        return parent[a];
+    }
+    
+    void union_set(int a, int b) {
+        a = find_set(a);
+        b = find_set(b);
+        if (a != b) {
+            parent[b] = a;
+        }
+    }
+    
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        make_set(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isConnected[i][j]) {
+                    union_set(i, j);
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (parent[i] == i) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
