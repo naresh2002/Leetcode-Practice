fa805c98c24242c83525c60e7f1babeacc81ba62
Naresh <naresh6436@gmail.com>
2024-10-28 16:18:07 +0530

    Time: 134 ms (93.38%), Space: 238.1 MB (91.88%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    int parent[200000];
+    void make_set(int n) {
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+    int find_set(int curr) {
+        if (parent[curr] == curr) {
+            return curr;
+        }
+        parent[curr] = find_set(parent[curr]);
+        return parent[curr];
+    }
+    void union_set(int a, int b) {
+        a = find_set(a);
+        b = find_set(b);
+        if (a != b) {
+            parent[b] = a;
+        }
+    }
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        make_set(n);
+        for (auto edge : edges) {
+            union_set(edge[0], edge[1]);
+        }
+        return find_set(source) == find_set(destination);
+    }
+};
