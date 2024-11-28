fc7efed527b60efe776f5017cf373b19498cbc6e
Naresh <naresh6436@gmail.com>
2024-11-28 19:36:49 +0530

    Time: 60 ms (42.91%), Space: 59.4 MB (56.02%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.cpp
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
-    // Good Explanation : https://leetcode.com/problems/minimum-height-trees/discuss/1630864/C%2B%2B-EASY-TO-SOLVE-oror-Detailed-Explanation-with-dry-run
+   // Good Explanation : https://leetcode.com/problems/minimum-height-trees/discuss/1630864/C%2B%2B-EASY-TO-SOLVE-oror-Detailed-Explanation-with-dry-run
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
         if (n == 1) {
             return {0};
         }
-        vector<int> degree (n, 0);
+        vector<int> degree(n, 0);
         vector<int> adj[n];
         for (auto edge : edges) {
-            degree[edge[0]]++;
-            degree[edge[1]]++;
             adj[edge[0]].push_back(edge[1]);
             adj[edge[1]].push_back(edge[0]);
+            degree[edge[0]]++;
+            degree[edge[1]]++;
         }
-        queue<int> degreeOneNodes;
+        queue<int> q;
         for (int i = 0; i < n; i++) {
             if (degree[i] == 1) {
-                degreeOneNodes.push(i);
+                q.push(i);
             }
         }
+        
         vector<int> ans;
-        while (!degreeOneNodes.empty()) {
-            int n = degreeOneNodes.size();
+        while(!q.empty()) {
+            int sz = q.size();
             ans.clear();
-            
-            // This is our level order traverse
-            while (n--) {
-                int curr = degreeOneNodes.front();
-                degreeOneNodes.pop();
+            while (sz--) {
+                int curr = q.front();
+                q.pop();
+                degree[curr]--;
                 ans.push_back(curr);
-                int currSize = adj[curr].size();
-                for (int i = 0; i < currSize; i++) {
-                    degree[adj[curr][i]]--;
-                    if (degree[adj[curr][i]] == 1) {
-                        degreeOneNodes.push(adj[curr][i]);
+                for (auto x : adj[curr]) {
+                    degree[x]--;
+                    if (degree[x] == 1) {
+                        q.push(x);
                     }
                 }
             }
