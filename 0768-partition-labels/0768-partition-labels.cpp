18ae9d8276c85a282ca9c34a8ee9b870f13c9fa5
Naresh <naresh6436@gmail.com>
2025-03-31 21:57:57 +0530

    Time: 0 ms (100%), Space: 8.8 MB (88.16%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.cpp
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -2,13 +2,7 @@ class Solution {
 public:
     vector<int> partitionLabels(string s) {
         int n = s.size();
-        vector<int> firstOccurence (26, -1);
         vector<int> lastOccurence (26, -1);
-        for (int i = 0; i < n; i++) {
-            if (firstOccurence[s[i] - 'a'] == -1) {
-                firstOccurence[s[i] - 'a'] = i;
-            }
-        }
         for (int i = n - 1; i >= 0; i--) {
             if (lastOccurence[s[i] - 'a'] == -1) {
                 lastOccurence[s[i] - 'a'] = i;
